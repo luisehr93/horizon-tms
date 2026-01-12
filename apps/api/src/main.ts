89b91ba5c85586622c0af3c91dae307d59c7daa8
luisehr93 <luisehr93@gmail.com>
2026-01-11 19:27:22 -0800

    Fix listen PORT for Railway

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,11 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Railway/Render/Heroku-style
   const port = Number(process.env.PORT) || 8080;
-
   await app.listen(port, '0.0.0.0');
+
   console.log(`Horizon TMS API running on port ${port}`);
 }
-
 bootstrap();
